197baa79ded838af6b7e2d45954021592726dce6
Brody Noonan <brodynoonan@outlook.com>
2016-10-26 10:48:13 +0800

    - Slightly improved the main menu

---
+++ Assets/Script/TitleScreenRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenRotate : MonoBehaviour {
+    
+    const int ROTATIONSPEED = 100;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, ROTATIONSPEED * Time.deltaTime);
+    }
+}
